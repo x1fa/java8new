c9991c655162f8773b5a7b794fc84d21578741ed
xifa <x1fa@qq.com>
2021-07-12 10:45:23 +0800

    first

---
+++ src/main/java/com/java8new1/Java8new1Application.java
@@ -0,0 +1,13 @@
+package com.java8new1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8new1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8new1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/java8new1/Java8new1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java8new1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8new1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
