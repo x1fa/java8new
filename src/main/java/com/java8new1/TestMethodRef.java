5866d283d3c102f1e700ba0741462f4789688780
xifa <x1fa@qq.com>
2021-07-14 08:27:03 +0800

    测试分支

--- src/main/java/com/java8new1/TestMethodRef.java
+++ src/main/java/com/java8new1/TestMethodRef.java
@@ -2,7 +2,13 @@ package com.java8new1;
 
 
 
+import org.junit.Test;
+
 import java.util.function.Consumer;
 
 public class TestMethodRef {
+    @Test
+    public  void method1(){
+        System.out.println("测试分支");
+    }
 }
